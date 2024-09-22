d7b148cc600844007e8f0221b68c21e9edb1c8b6
Rishabh Porwal <rishabhporwal95@gmail.com>
2024-09-22 19:23:43 +0530

    uploaded data to mongodb

---
+++ src/constant/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+AWS_BUCKET_NAME="wafer-fault"
+MONGO_DATABASE_NAME="pwskills"
+MONGO_COLLECTION_NAME="waferfault"
+
+TARGET_COLUMN="quality"
+MONGO_DB_URL="mongodb+srv://rishabh:JSZ903tgcckHBghJ@cluster0.tjg74.mongodb.net/?retryWrites=true&w=majority"
+
+MODEL_FILE_NAME="model"
+MODEL_FILE_EXTENSION=".pkl"
